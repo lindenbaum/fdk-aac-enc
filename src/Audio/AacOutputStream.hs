8c5a0eacedf9f9159c1cf7d3a5ea9ca8b0c78599
Sven Heyll <sven.heyll@lindenbaum.eu>
2016-09-21 07:23:51 +0200

    Require user provided availabilityStartTime

--- src/Audio/AacOutputStream.hs
+++ src/Audio/AacOutputStream.hs
@@ -24,37 +24,47 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Builder as BB
 import Text.Printf
+import           Data.Time.Clock               (UTCTime)
 
 newtype StreamId = StreamId {unStreamId :: Word64}
   deriving (Integral,Num,Eq,Ord,Show,Enum,Real)
 
 data Context =
   Context
-    { faHandle          :: !AacEncoderHandle
-    , faOutVec          :: !(VM.IOVector C.CUChar)
-    , faStream          :: !Mp4.StreamingContext
-    , faId              :: !StreamId
-    , faSegmentDuration :: !Word32
+    { faHandle                          :: !AacEncoderHandle
+    , faOutVec                          :: !(VM.IOVector C.CUChar)
+    , faStream                          :: !Mp4.StreamingContext
+    , faId                              :: !StreamId
+    , faSegmentDuration                 :: !Word32
     , faConsumedButUnencodedSampleCount :: !Word32
-    --    , faCache    :: !DashStreamCache
     }
 
-streamOpen :: StreamId -> Word32 -> IO (Maybe (InitSegment, Context))
-streamOpen sId segmentDurationMillis = do
-  (initBuilder, st) <- Mp4.streamInit (printf "TalkFlow:%0.16X" (unStreamId sId))
-                                      segmentDurationMillis
-                                      False
-                                      Mp4.SF16000
-                                      Mp4.SingleChannel
-  aacEncoderNew (Mp4.getStreamConfig st) >>= mapM (onEncoderStarted initBuilder st)
+streamOpen
+  :: MonadIO m
+  => StreamId -> UTCTime -> Word32 -> m (Maybe (InitSegment, Context))
+streamOpen sId availabilityStartTime segmentDurationMillis =
+    liftIO lowLevelInit >>= mapM createContext
   where
-    onEncoderStarted initBuilder st h = do
-      o <- VM.new 768
+    lowLevelInit = do
+      let (initBuilder, st) =
+            Mp4.streamInitUtc
+            (printf "TalkFlow:%0.16X" (unStreamId sId))
+            availabilityStartTime
+            segmentDurationMillis
+            False
+            Mp4.SF16000
+            Mp4.SingleChannel
+      fmap (initBuilder, st, ) <$> aacEncoderNew (Mp4.getStreamConfig st)
+
+    createContext (initBuilder, st, h) = do
+      o <- liftIO $ VM.new 768
       let !i = InitSegment (BL.toStrict (BB.toLazyByteString initBuilder))
       return (i, Context h o st sId segmentDurationMillis 0)
 
-streamClose :: Context -> IO (Maybe Segment)
-streamClose Context{..} = do
+streamClose
+  :: MonadIO m
+  => Context -> m (Maybe Segment)
+streamClose Context{..} = liftIO $ do
     aacEncoderClose faHandle  -- TODO error handling
     let (!msegment, faStream') = Mp4.streamFlush faStream
     case msegment of
@@ -79,7 +89,8 @@ streamEncodePcm !inVecFrozen !callback !ctxIn =
       do (!ok, !cons, !inVec', !outVec')
             <- liftIO $ aacEncoderEncode faHandle inVec faOutVec
          if not ok
-           then do liftIO $ printf "\n\n\n*** Encoder error in stream: %16X\n\n\n" (unStreamId faId)
+           then do liftIO $ printf "\n\n\n*** Encoder error in stream: %16X\n\n\n"
+                            (unStreamId faId)
                    -- TODO error handling
                    return Nothing
            else do
@@ -91,7 +102,9 @@ streamEncodePcm !inVecFrozen !callback !ctxIn =
                  Just vo -> do
                    vo' <- liftIO $ V.freeze vo
                    let !voBS = vectorToByteString vo'
-                       (!msegment, !faStream') = Mp4.streamNextSample totalConsumed voBS faStream
+                       (!msegment, !faStream') = Mp4.streamNextSample totalConsumed
+                                                                      voBS
+                                                                      faStream
                    case msegment of
                      Just !segment -> do
                        let !strictSeg = BL.toStrict (BB.toLazyByteString segment)
@@ -101,7 +114,9 @@ streamEncodePcm !inVecFrozen !callback !ctxIn =
                    return faStream'
              let !nextCtx = ctx { faStream = nextFaStream
                                 , faConsumedButUnencodedSampleCount =
-                                  if isJust outVec' then 0 else totalConsumed }
+                                  if isJust outVec'
+                                     then 0
+                                     else totalConsumed}
              case inVec' of
                Nothing -> do -- Input consumed, done here
                  return (Just nextCtx)
@@ -123,12 +138,12 @@ newtype InitSegment =
 
 instance Show InitSegment where
   show (InitSegment !d) =
-    printf "* INIT SEGMENT - size: %14d" (BS.length d)
+    printf "INIT SEGMENT - size: %14d" (BS.length d)
 
 
 instance Show Segment where
   show (Segment !s !t !d) =
-    printf "seq: %14d - time: %14d - size: %14d" s t (BS.length d)
+    printf "SEGMENT - seq: %14d - time: %14d - size: %14d" s t (BS.length d)
 
 mkSegment :: BS.ByteString -> Mp4.StreamingContext -> Segment
 mkSegment !b !sc =
